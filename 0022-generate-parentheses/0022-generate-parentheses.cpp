1f2ea26bb43a4830b698621949b3fa236a5cd61d
Aaditya sharma <aadityasharma25dec@gmail.com>
2026-08-17 22:24:17 +0530

    Time: 0 ms (100%), Space: 16.1 MB (26.18%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string>ans;
+    void backtrack(string s,int open,int close,int n){
+        if(open==n&&close==n){
+            ans.push_back(s);
+            return;
+        }
+        if(open<n){
+            backtrack(s+"(",open+1,close,n);
+        }
+        if(close<open){
+            backtrack(s+")",open,close+1,n);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        ans.clear();
+        backtrack("",0,0,n);
+        return ans;
+    }
+};
