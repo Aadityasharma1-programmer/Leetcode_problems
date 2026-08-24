268b64e70f30858eacb9de23d96832ea0fcc53ea
Aaditya sharma <aadityasharma25dec@gmail.com>
2026-08-24 15:47:45 +0530

    Time: 0 ms (100%), Space: 10 MB (53.22%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left=0;
+        int n=s.length()-1;
+        int right=s.length()-1;
+        while(left<right){
+            while(left<right && !isalnum(s[left])){
+                left++;
+            }
+            while(right>left && !isalnum(s[right])){
+                right--;
+            }
+            if(tolower(s[left])!=tolower(s[right])){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+
+    }
+};
